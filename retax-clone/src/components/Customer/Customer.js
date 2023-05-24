ace24392294fefb05d65da6af786093116129d87
Swagh001 <shaileshwagh300@gmail.com>
2023-05-24 19:55:52 +0530

    file added with some structure

---
+++ retax-clone/src/components/Customer/Customer.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Customer=()=>{
+
+    return <div>
+        <div></div>
+        <div>
+            <div>
+                <div><h1>Customers</h1></div>
+
+                <div>
+                    <div>serach bpc and some paraMeter</div>
+                    <div>sorting</div>
+                </div>
+            </div>
+
+            <div>
+
+
+                
+            </div>
+
+        </div>
+    </div>
+}
