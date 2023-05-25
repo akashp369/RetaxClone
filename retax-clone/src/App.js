a33c34900dcfdc07f8aa11df98f6284b045baa67
unknown <shaileshpanghate29816@gmail.com>
2023-05-25 14:52:06 +0530

    abc

--- retax-clone/src/App.js
+++ retax-clone/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-// import Employee from './components/Employee/Employee';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Retax E- Commerce Clone</h1>
-      {/* <Employee /> */}
+    
     </div>
   );
 }
