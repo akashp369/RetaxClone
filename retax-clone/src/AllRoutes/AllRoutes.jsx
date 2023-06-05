6e35d1365597013c260943db0c74a72673135295
akashp369 <aakashprajapat59@gmail.com>
2023-06-05 13:57:11 +0530

    final update

--- retax-clone/src/AllRoutes/AllRoutes.jsx
+++ retax-clone/src/AllRoutes/AllRoutes.jsx
@@ -6,6 +6,7 @@ import Login from '../components/Login/Login';
 import Main from '../components/Login/Main';
 import Employee from '../components/Employee/Employee';
 import EmployeeDetails from '../components/Employee/EmployeeDetails';
+import { Customer } from '../components/Customer/Customer';
 export default function AllRoutes({isLogin, update_login}) {
   const navigate=useNavigate();
   if(isLogin==false){
@@ -17,7 +18,7 @@ export default function AllRoutes({isLogin, update_login}) {
     >
     <Routes >
       <Route path={'/dashboard'} element={<Home update_login={update_login} isLogin={isLogin}/>} />
-      <Route path={'/customer'} element={<Home />} /> {/* shailesh Wagh*/}
+      <Route path={'/customer'} element={<Customer />} /> {/* shailesh Wagh*/}
       <Route path={'/employee'} element={<Employee />} /> {/* shailesh panghate*/}
       <Route path={'/employee/:emp_id'} element={<EmployeeDetails  />} /> {/* shailesh panghate*/}
       <Route path={'/cases'} element={<Home />} /> {/* sweksha patel*/}
